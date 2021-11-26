3b9b13d50e3091148dc50517099debc46d939733
Anderson Aquino <andersonaquino93@gmail.com>
2021-11-25 23:54:00 -0300

    add delete class

---
+++ src/TestaRemocao.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = new ConnectionFactory().getConnection();
+		Statement stmt = connection.createStatement();
+
+		stmt.execute("DELETE FROM produto WHERE ID > 3");
+		Integer linhasModificadas = stmt.getUpdateCount();
+		
+		System.out.printf("Quantidade de linhas modificadas: %s", linhasModificadas);
+		
+		connection.close();
+
+	}
+
+}
