158a01f86c38e6bd8fe123d0825b402de0f643e1
Anderson Aquino <andersonaquino93@gmail.com>
2021-11-25 23:47:32 -0300

    add insert class

---
+++ src/TestaInsercao.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection connection = new ConnectionFactory().getConnection();
+		Statement stmt = connection.createStatement();
+
+		stmt.execute("INSERT INTO produto (nome,descricao) VALUES ('Mouse', 'Mouse sem fio')",
+				Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rs = stmt.getGeneratedKeys();
+		
+		while(rs.next()) {
+			Integer id = rs.getInt(1);
+			System.out.printf("O ID do produto cadastrado foi %s", id);
+		}
+
+		connection.close();
+
+	}
+
+}
