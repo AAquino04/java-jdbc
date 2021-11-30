2e484e1457fe6af1b57f58e37442fab7e6a97136
Anderson Aquino <andersonaquino93@gmail.com>
2021-11-30 00:10:53 -0300

    refactor insertWithParam class to use try-with-resources

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -7,41 +7,35 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		Connection connection = new ConnectionFactory().getConnection();
-		connection.setAutoCommit(false);
-		PreparedStatement stmt = connection.prepareStatement("INSERT INTO produto (nome, descricao) VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-		
-		try {
-			adicionarVariavel("Monitor", "Monitor 24 polegadas", stmt);
-			adicionarVariavel("Rádio Analógico", "Rádio analógico de bateria", stmt);
+		try (Connection connection = new ConnectionFactory().getConnection()) {
 
-			connection.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
-		} finally {
-			stmt.close();
-			connection.close();
+			connection.setAutoCommit(false);
+
+			try (PreparedStatement stmt = connection.prepareStatement(
+					"INSERT INTO produto (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);) {
+				adicionarVariavel("Monitor", "Monitor 24 polegadas", stmt);
+				adicionarVariavel("Rádio Analógico", "Rádio analógico de bateria", stmt);
+
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
 		}
-
 	}
 
 	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stmt) throws SQLException {
 		stmt.setString(1, nome);
 		stmt.setString(2, descricao);
-		
+
 		stmt.execute();
-		
-		ResultSet rs = stmt.getGeneratedKeys();
 
-		while (rs.next()) {
-			Integer id = rs.getInt(1);
-			System.out.printf("O ID do produto cadastrado foi %s \n", id);
+		try (ResultSet rs = stmt.getGeneratedKeys()) {
+			while (rs.next()) {
+				Integer id = rs.getInt(1);
+				System.out.printf("O ID do produto cadastrado foi %s \n", id);
+			}
 		}
-
-		rs.close();
 	}
-
 }
