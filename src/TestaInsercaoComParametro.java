1b21d0190832b9f1ee44f1c12d640b29f8c762a7
Anderson Aquino <andersonaquino93@gmail.com>
2021-11-29 23:50:41 -0300

    refactor insert with param class to treat exceptions and execute rollback

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,26 +8,40 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = new ConnectionFactory().getConnection();
+		connection.setAutoCommit(false);
 		PreparedStatement stmt = connection.prepareStatement("INSERT INTO produto (nome, descricao) VALUES (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
+		
+		try {
+			adicionarVariavel("Monitor", "Monitor 24 polegadas", stmt);
+			adicionarVariavel("Rádio Analógico", "Rádio analógico de bateria", stmt);
 
-		String nome = "Teclado";
-		String descricao = "Teclado Mecânico Preto";
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			connection.rollback();
+		} finally {
+			stmt.close();
+			connection.close();
+		}
 
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stmt) throws SQLException {
 		stmt.setString(1, nome);
 		stmt.setString(2, descricao);
-
+		
 		stmt.execute();
-
+		
 		ResultSet rs = stmt.getGeneratedKeys();
 
 		while (rs.next()) {
 			Integer id = rs.getInt(1);
-			System.out.printf("O ID do produto cadastrado foi %s", id);
+			System.out.printf("O ID do produto cadastrado foi %s \n", id);
 		}
 
-		connection.close();
-
+		rs.close();
 	}
 
 }
