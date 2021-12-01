21509c0ccd09c86ea1c3b394e43f6ce99842dccd
Anderson Aquino <andersonaquino93@gmail.com>
2021-12-01 00:00:52 -0300

    add connection pool

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,25 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	public DataSource dataSource; 
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?userTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("fM@031011");
+		
+		this.dataSource = comboPooledDataSource;
+	}
+	
 	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?userTimezone=true&serverTimezone=UTC",
-				"root", "fM@031011");
+		return this.dataSource.getConnection();
 	}
 
 }
